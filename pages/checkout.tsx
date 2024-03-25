0616a986665fe923c451b09c58651a6610466942
DimasNuryadin <dimasnuryadinn@gmail.com>
2024-03-26 00:16:37 +0700

    handle private route

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -3,8 +3,17 @@ import CheckoutConfirmation from "../components/organisms/CheckoutConfimation";
 import CheckoutDetail from "../components/organisms/CheckoutDetail";
 import CheckoutItem from "../components/organisms/CheckoutItem";
 import Link from "next/link";
+import { jwtDecode } from "jwt-decode";
+import { JWTPayloadTypes, UserTypes } from "../services/data-types";
 
-export default function Checkout() {
+interface CheckoutProps {
+  user: UserTypes;
+}
+
+export default function Checkout(props: Readonly<CheckoutProps>) {
+  const { user } = props;
+  console.log('user', user)
+
   return (
     <section className="checkout mx-auto pt-md-100 pb-md-145 pt-30 pb-30">
       <div className="container-fluid">
@@ -24,4 +33,38 @@ export default function Checkout() {
       </div>
     </section>
   )
+}
+
+// Cek user sudah login
+export async function getServerSideProps({ req }: any) {      // contex berisi req, res, dll
+  const { token } = req.cookies;
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/sign-in',
+        permanetn: false,
+      }
+    }
+  }
+  console.log('token : ', token);   // Console hanya bisa dilihat pada console
+
+  // atob tidak bisa digunakan di server jadi pakai fungsi Buffer yang sudah disediakan oleh node
+  const jwtToken = Buffer.from(token, 'base64').toString('ascii');
+
+  // jwt_decode
+  const payload: JWTPayloadTypes = jwtDecode(jwtToken);
+
+  const userFromPayload: UserTypes = payload.player;
+
+  // Image
+  if (userFromPayload.avatar) {
+    const IMG = process.env.NEXT_PUBLIC_IMG;
+    userFromPayload.avatar = `${IMG}/${userFromPayload.avatar}`;
+  }
+
+  return {
+    props: {
+      user: userFromPayload,   // Kirim data payload
+    }
+  }
 }
